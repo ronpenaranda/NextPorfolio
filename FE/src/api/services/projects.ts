ad82fa13b9b6a524854f831c001ba1fa7a9619e9
Ron Penaranda <115511933+charlespenaranda@users.noreply.github.com>
2024-11-26 22:14:57 +0800

    add minimal changes
    add minimal changes

--- FE/src/api/services/projects.ts
+++ FE/src/api/services/projects.ts
@@ -1,14 +1,41 @@
 import supabase from "@/db/supabase";
 
-export const getProjects = async () => {
-    
-    let { data: projects, error } = await supabase
-    .from('projects')
-    .select("*")
-
-    if(error){
-        console.error(error)
-    }
-        
-    return projects;
+interface Project {
+  id: number;
+  created_at: string;
+  title: string;
+  content: string;
+  technology: string[];
+  url: string;
 }
+
+class ProjectsService {
+  static async getAllProjects(): Promise<Project[]> {
+    const { data: projects, error } = await supabase
+      .from("projects")
+      .select("*");
+
+    if (error) {
+      console.error("Error fetching projects:", error);
+      throw error;
+    }
+
+    return projects || [];
+  }
+
+  static async getProjectsByName(name: string): Promise<Project[]> {
+    const { data: projects, error } = await supabase
+      .from("projects")
+      .select("*")
+      .eq("title", name);
+
+    if (error) {
+      console.error("Error fetching project by name:", error);
+      throw error;
+    }
+
+    return projects || [];
+  }
+}
+
+export default ProjectsService;
--- FE/src/app/(main)/projects/page.tsx
+++ FE/src/app/(main)/projects/page.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import CardApp from '@/components/app-card';
-import { getProjects } from '@/api/services/projects';
+import ProjectsService from '@/api/services/projects';
 
 const Page = async () => {
 
-  const project = await getProjects()
-
+  const project = await ProjectsService.getAllProjects()
 
   return (
     <div className="min-w-[500px] max-w-full overflow-auto items-center justify-center bg-white p-6 py-10">
       <div className='py-4'>
       <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"> List of Projects </h2>
-      <p className="mt-4 text-gray-500">
+      <p className="mt-4 text-gray-500 text-sm">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
       </p>
       </div>
