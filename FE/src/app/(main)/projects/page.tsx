073dc07078cca6ee0ed4a9220fad3877b9167b49
Ron Penaranda <115511933+charlespenaranda@users.noreply.github.com>
2024-11-26 15:54:07 +0800

    Update page.tsx

--- FE/src/app/(main)/projects/page.tsx
+++ FE/src/app/(main)/projects/page.tsx
@@ -4,18 +4,18 @@ import { projects } from '@/api/dummyapi'
 
 const Page = () => {
   return (
-    <div className="min-w-[500px] max-w-full overflow-auto items-center justify-center bg-white p-6">
+    <div className="min-w-[500px] max-w-full overflow-auto items-center justify-center bg-white p-6 py-10">
       <div className='py-4'>
-      <h2 className="text-2xl md:text-2xl text-gray-900 mb-2"> List of Projects </h2>
-      <p className="mt-1 text-sm leading-6 text-gray-600">
+      <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"> List of Projects </h2>
+      <p className="mt-4 text-gray-500">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
       </p>
       </div>
-      <div className='flex flex-wrap gap-4'>
-        {projects.map((item, index) => {
+      <div className='flex flex-wrap gap-4 py-10'>
+        {projects?.map((item, index) => {
           return(
             <div key={index}>
-            <CardApp content={item}/>
+              <CardApp content={item}/>
             </div>
           )
         })}
